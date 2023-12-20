7918f456d68acbfb332df8d89784802b5fdf3895
saudgh07 <109569540+saudgh07@users.noreply.github.com>
2023-12-20 11:42:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 
 st.title("Excel Update App")
+save_data = st.button("Save File")
 df = pd.read_csv("names.csv")
-st.header ("Random Data")
+st.header("Random Data")
 st.write(df)
-st. sidebar.header ("Options")
 
+
+st.sidebar.header ("Options")
 options_form = st.sidebar.form("options_form")
 user_name = options_form.text_input("Name")
 user_age = options_form.text_input("Age")
