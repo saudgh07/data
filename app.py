53ed4a16a89f6420eb2eb9f8c2609ab3d2a63309
saudgh07 <109569540+saudgh07@users.noreply.github.com>
2023-12-20 11:27:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ user_name = options_form.text_input ("Name")
 user_age = options_form. text_input ("Age")
 add_data = options_form.form_submit_button()
 if add_data:
-  new_data = {"name": user_name,"age": int(user_age)}
+  new_data = {"name": user_name, "age": int(user_age)}
   df = df.append(new_data, ignore_index=True)
-  df.to_csv("names.csv", index-False)
+  df.to_csv("names.csv", index=False)
